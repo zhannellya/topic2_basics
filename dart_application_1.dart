289ac8f8a5b7e37792dcf8bd9fc483e5257295bd
Rysbai Zhanel <120437053+zhannellya@users.noreply.github.com>
2023-09-30 10:25:45 +0300

    Add files via upload

---
+++ dart_application_1.dart
@@ -0,0 +1,66 @@
+import 'dart:math';
+// import 'dart:io';
+
+void main() {
+  //Task1
+  var x = 2;
+  print(7 * (x * x) - 3 * x + 6);
+
+  var a = 2;
+  print(12 * (a * a) + 7 * a - 16);
+
+  //Task2
+  print(pow(a, 2) + 10 / sqrt(pow(a, 2) + 1));
+
+  //Task3
+  var y = 1;
+  print(pow(x, 3) - 2.5 * x * y + 1.78 * pow(x, 2) - 2.5 * y + 1);
+
+  var b = 1;
+  print(3.56 * a + pow(b, 3) - 5.8 * pow(b, 2) + 3.8 * a - 1.5);
+
+  //Task4
+  print(sqrt(2 * a + sin(3 * a) / 3.56));
+
+  //Task5
+  var san = 3;
+  print(san * 4);
+
+  //Task6
+  int ab = 20;
+  int ba = 23;
+
+  double arithMetic = (ab + ba) / 2;
+  double geoMetric = sqrt(ab * ba);
+  print("$arithMetic");
+  print("$geoMetric");
+
+  //Bonus task1
+  int n = 3600;
+  int hours = (n ~/ 3600);
+  print(hours);
+
+  int minutes = (n % 3600 ~/ 60);
+  print(minutes);
+
+  int seconds = 60 - (n % 60);
+  print(seconds);
+
+  //Bonus task2
+  int k = 1;
+  List<String> daysOfWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+  int dayIndex = (k - 1) % 7;
+  String day = daysOfWeek[dayIndex];
+  int value = daysOfWeek.indexOf(day);
+
+  print('Day of the week for day $k: $day');
+  print('Assigned integer value for $day: $value');
+}
